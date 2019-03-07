f309cfbe03d67499cb80b45bf2f07b837aa6c2e0
mistertandon <enggparveshtandon@gmail.com>
2019-03-07 16:07:28 +0530

    feat: Javascript bind function
    Develop code to exhibit call method functionality in javascript.

--- 11_function/01_call/01_call.js
+++ 11_function/01_call/01_call.js
@@ -19,4 +19,36 @@
 
     let sumResult = getSum.call(addendParameters, 5);
     console.log(sumResult)
+
+    var getSumImplicitly = function () {
+
+        let addends = [].slice.call(arguments)
+
+        let sumResult = addends.reduce((accumulator, currentValue) => {
+
+            return accumulator + currentValue;
+        })
+        console.log(sumResult)
+    }
+
+    getSumImplicitly(1, 23, 4, 5);
+
 })()
+
+
+    (function () {
+
+        let Mammel = function (legs) {
+
+            this.legs = legs;
+        }
+
+        let Cat = function (legs, isDomistiated) {
+
+            Mammel.call(this, legs)
+            this.isDomistiated = isDomistiated;
+        }
+
+        let lion = new Cat(4, false);
+        console.log(lion);
+    })();
