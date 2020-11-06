90147c0222a2c169fe4366b8e53fbf49b1a0d201
mistertandon <enggparveshtandon@gmail.com>
2020-11-06 22:52:05 +0530

    feat: Add quick note to null and undefined.

---
+++ 22_null_undefined/basic.js
@@ -0,0 +1,8 @@
+var demo;
+console.log(demo); //shows undefined
+console.log(typeof demo); //shows undefined
+
+
+var demo = null;
+console.log(demo); //shows null
+console.log(typeof demo); //shows object
